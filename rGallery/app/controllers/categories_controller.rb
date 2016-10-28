ff413c1b8b84f85e6c75c0fb4c0acde17ff6e588
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-10-28 16:04:37 +0100

    Finish ability to add/edit categories (forgot to add the create method...)

--- rGallery/app/controllers/categories_controller.rb
+++ rGallery/app/controllers/categories_controller.rb
@@ -4,6 +4,14 @@ class CategoriesController < ApplicationController
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = 'Category Created Successfully'
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -16,5 +24,11 @@ class CategoriesController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
